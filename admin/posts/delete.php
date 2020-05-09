0a4ef35dcc3fa4233925213eb1a038058c19663b
CodyPerdue <48223429+CodyPerdue@users.noreply.github.com>
2020-05-08 23:33:08 -0400

    Delete delete.php

--- admin/posts/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('../../settings.php');
-require_once('../Admin.php');
-Admin::requireAdmin('../../index.php');
-require_once('../../Post.php');
-$post=new Post();
-$post->delete($_GET['id']);
-header('location:index.php');
