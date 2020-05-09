16a02218c68a34101bd4d7dbd15a68935f75eff5
CodyPerdue <48223429+CodyPerdue@users.noreply.github.com>
2020-05-08 23:32:28 -0400

    Delete delete.php

--- admin/users/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('../../settings.php');
-require_once('../Admin.php');
-Admin::requireAdmin('../../index.php');
-require_once('../../User.php');
-$user=new User();
-$user->delete($_GET['id']);
-header('location:index.php');
